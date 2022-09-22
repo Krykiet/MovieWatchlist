28928cd1e8e35fb21c3cabf69abe3aac2db1d9b7
Krystian Chudzik <chudzik.kd@gmail.com>
2022-09-22 21:41:54 +0200

    added index for movie timestamp

--- venv/database.py
+++ venv/database.py
@@ -30,6 +30,8 @@ SELECT_WATCHED_MOVIES = """SELECT movies.* FROM movies
     WHERE users.username = ?;"""
 INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?)"
 SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE ?;"
+CREATE_RELEASE_INDEX = "CREATE INDEX IF NOT EXISTS idx_movies_release ON movies(release_timestamp);"
 
 
 connection = sqlite3.connect("data.db")
@@ -39,6 +41,7 @@ def create_tables():
         connection.execute(CREATE_MOVIES_TABLE)
         connection.execute(CREATE_USERS_TABLE)
         connection.execute(CREATE_WATCHED_TABLE)
+        connection.execute(CREATE_RELEASE_INDEX)
 
 def add_user(username):
     with connection:
@@ -56,6 +59,12 @@ def get_movies(upcoming=False):
             cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
         else:
             cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
+
+def search_movies(search_term):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIES, (f"%{search_term}%",))
         return cursor.fetchall()
 
 def watch_movie(username, movie_id):
