8e2c3679e9459314e8fb96ab1718dc342a0c9e8b
Krystian Chudzik <chudzik.kd@gmail.com>
2022-09-22 19:48:35 +0200

    app changes to fit database rearrangement

--- app.py
+++ app.py
@@ -35,10 +35,19 @@ def print_movie_list(heading, movies):
         print(f"{movie[0]} (on {human_date}")
     print("---- \n")
 
+
+def print_watched_movie_list(username, movies):
+    print(f"-- {username}'s watched movies --")
+    for movie in movies:
+        print(f"{movie[1]}")
+    print("---- \n")
+
+
 def prompt_watch_movie():
     username = input("Username: ")
     movie_title = input("Enter movie title you've watched: ")
     database.watch_movie(username, movie_title)
+
 
 while (user_input := input(menu)) != "6":
     if user_input == "1":
@@ -52,5 +61,8 @@ while (user_input := input(menu)) != "6":
     elif user_input == "4":
         prompt_watch_movie()
     elif user_input == "5":
-        movies = database.get_watched_movies()
-        print_movie_list("Watched", movies)
+        username = input("Username: ")
+        movies = database.get_watched_movies(username)
+        print_watched_movie_list(username, movies)
+    else:
+        print("Invalid input, please try again!")
